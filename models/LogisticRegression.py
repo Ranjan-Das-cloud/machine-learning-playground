a65061036d60f6c12d85c32b28d0dbfe78c5fcb2
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-07-18 17:20:55 +0530

    Renamed a hyper-parameter of the models

--- models/LogisticRegression.py
+++ models/LogisticRegression.py
@@ -19,7 +19,7 @@ def logistic_regression_param_selector():
         penalties = ["l1"]
 
     penalty = st.selectbox("penalty", options=penalties)
-    C = st.number_input("C", 0.1, 2.0, 1.0, 0.1)
+    C = st.number_input("C (Inverse of regularization strength)", 0.1, 2.0, 1.0, 0.1)
     C = np.round(C, 3)
     max_iter = st.number_input("max_iter", 100, 2000, step=50, value=100)
 
